c62f25639362260595fcd8361ebaf3203d41220c
Pauline Audrey Ongchan <214192204+pauline-ongchan@users.noreply.github.com>
2026-08-16 19:14:05 -0700

    Add: counting-words-with-a-given-prefix - submission-1

---
+++ Data Structures & Algorithms/counting-words-with-a-given-prefix/submission-1.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+    
+class PrefixTree:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, w: str, length: int) -> None:
+        cur = self.root
+        for i in range(length):
+            if w[i] not in cur.children:
+                cur.children[w[i]] = TrieNode()
+            cur = cur.children[w[i]]
+            cur.count += 1
+    def count(self, pref:str) -> int:
+        cur = self.root
+        for c in pref:
+            if c not in cur.children:
+                return 0
+            cur = cur.children[c]
+        return cur.count
+
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        prefix_tree = PrefixTree()
+
+        for w in words:
+            if len(w) >= len(pref):
+                prefix_tree.add(w, len(pref))
+
+        return prefix_tree.count(pref)
+        
+
+
+        
